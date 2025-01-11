32d6c64df159fa6b301b154eff04809b2b915988
waldbaan <151732353+waldbaan@users.noreply.github.com>
2025-01-11 17:17:35 +0100

    version 2.o s Please enter the commit message for your changes. Lines starting

--- FRONDEND/ITM-EASY-DEVOPS/Program.cs
+++ FRONDEND/ITM-EASY-DEVOPS/Program.cs
@@ -1,3 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, ITM-EASY-DEVOPS");
-Console.WriteLine("The current time is " + DateTime.Now);
+Console.WriteLine("ITM-easy-devops");
